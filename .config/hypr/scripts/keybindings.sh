883c311679a8e20cd691a718bf2cd4659d95c8f3
sean <heavydragon99@outlook.com>
2025-12-23 20:48:24 +0100

    Updated hypr

--- .config/hypr/scripts/keybindings.sh
+++ .config/hypr/scripts/keybindings.sh
@@ -1,39 +1,25 @@
 #!/usr/bin/env bash
-#    __            __   _         ___
-#   / /_____ __ __/ /  (_)__  ___/ (_)__  ___ ____
-#  /  '_/ -_) // / _ \/ / _ \/ _  / / _ \/ _ `(_-<
-# /_/\_\\__/\_, /_.__/_/_//_/\_,_/_/_//_/\_, /___/
-#          /___/                        /___/
-#
 
-# -----------------------------------------------------
-# Get keybindings location based on variation
-# -----------------------------------------------------
-config_file=$(<~/.config/hypr/conf/keybinds.conf)
-config_file=${config_file//source = ~//home/$USER}
-
-# -----------------------------------------------------
 # Path to keybindings config file
-# -----------------------------------------------------
+config_file="$HOME/.config/hypr/conf/keybinds.conf"
 echo "Reading from: $config_file"
 
-keybinds=$(awk -F'[=#]' '
+# Parse keybindings
+keybinds=$(awk -F'=' '
     $1 ~ /^bind/ {
-        # Replace the string "$mainMod" with "SUPER" (for the super key)
-        gsub(/\$mainMod/, "SUPER", $0)
-
-        # Remove "bind" and extra spaces, if any, at the beginning of the line
-        gsub(/^bind[[:space:]]*=+[[:space:]]*/, "", $0)
-
-        # Split the keybinding part (e.g., "Mod1,Return") using a comma
-        split($1, kbarr, ",")
-
-        # Format the keybinding and associated command and prepare for output:
-        # Concatenate the two keybinding keys (e.g., "Mod1" + "Return") and append the command
-        print kbarr[1] "  + " kbarr[2] "\r" $2
+        line=$0
+        gsub(/\$mainMod/, "SUPER", line)
+        gsub(/^bind[[:space:]]*/, "", line)
+        split(line, arr, "=")
+        key=arr[1]; cmd=arr[2]
+        gsub(/^[[:space:]]+|[[:space:]]+$/, "", key)
+        gsub(/^[[:space:]]+|[[:space:]]+$/, "", cmd)
+        print key "  →  " cmd
     }
 ' "$config_file")
 
+# Optional tiny delay
 sleep 0.2
 
+# Show in Rofi
 rofi -dmenu -i -markup -eh 2 -replace -p "Keybinds" -config ~/.config/rofi/config-compact.rasi <<<"$keybinds"
